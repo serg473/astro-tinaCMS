8db2c98689d1f91788296a1c20f1d258a454409d
serg473 <sergeidsi473@gmail.com>
2025-03-04 19:53:55 +0300

    fix: change type code

--- tina/config.ts
+++ tina/config.ts
@@ -3,20 +3,10 @@ import { BlogCollection } from "./collections/blog";
 import { GlobalConfigCollection } from "./collections/global-config";
 import { PageCollection } from "./collections/page";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
-
 export default defineConfig({
-  branch,
-
-  // Get this from tina.io
-  clientId: process.env.PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  branch: "main",
+  clientId: "1de24bf3-0670-4af7-ac85-0cfca3ad7902",
+  token: "6895fa6c887cb4d203ced878326e6908c6fbc55a",
 
   build: {
     outputFolder: "admin",
@@ -30,10 +20,6 @@ export default defineConfig({
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
-    collections: [
-      BlogCollection,
-      PageCollection,
-      GlobalConfigCollection,
-    ],
+    collections: [BlogCollection, PageCollection, GlobalConfigCollection],
   },
 });
