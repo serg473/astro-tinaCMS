e8902ae9850c8ea3c767113acb45a316d6f29562
serg473 <sergeidsi473@gmail.com>
2025-03-05 00:53:24 +0300

    USE env vars

--- tina/config.ts
+++ tina/config.ts
@@ -4,7 +4,7 @@ import { GlobalConfigCollection } from "./collections/global-config";
 import { PageCollection } from "./collections/page";
 
 export default defineConfig({
-  branch: process.env.TINA_BRANCH,
+  branch: process.env.VERCEL_GIT_COMMIT_REF,
   clientId: process.env.TINA_CLIENT_ID,
   token: process.env.TINA_TOKEN,
 
